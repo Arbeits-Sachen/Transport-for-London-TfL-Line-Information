9c0c9e3899dfd9f97fdfe92615b46684677fe8db
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-12-02 12:10:53 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import { useState, useEffect } from "react";
 import CreateData from "./CreateData"
 
-function App() 
+function App()
 {
+  const [Data, setData] = useState([]);
+
+  useEffect(() =>
+  {
+    fetch("https://api.tfl.gov.uk/Line/Meta/Modes")
+      .then(response => response.json())
+      .then(data => setData(data))
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hallo!</h1>
-        <CreateData/>
+        <h1>Transport For Londom Line Information</h1>
+        <CreateData Data={Data} />
       </header>
     </div>
   );
